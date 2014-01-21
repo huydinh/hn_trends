0ff5254aa61952800d732e4c6f35b81df5cde16f
huyd <huy.dinh@eastagile.com>
2014-01-21 17:58:25 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HnTrends::Application.config.secret_key_base = '11fff5dcb907d34923fb392f15682fec32ef556899a52fe55d5a3d3a1b0ba70bf5f9c2946dbe2ddb37f8939fcb6ebd5d6e6b5c6b5c15c9a116621900efa523ed'
