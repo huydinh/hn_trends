87e43165415f7ad85b8b09bef486b91947019f7c
huyd <huy.dinh@eastagile.com>
2014-01-23 17:44:31 +0700

    style the hottest page

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -5,4 +5,13 @@ module WordsHelper
       ['2 last week', 14],
       ['last month', 28] ]
   end
+
+  def width_for_point(point)
+    point = point.to_i
+
+    point = point > 10 ? 10 : point
+    point = point <= 0 ? 1 : point
+
+    "item-#{point}"
+  end
 end
