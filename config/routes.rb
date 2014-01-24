bc6f250b536608cc89b35a7f33c9aa5874faf652
huyd <huy.dinh@eastagile.com>
2014-01-24 16:45:51 +0700

    temporarily disable admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HnTrends::Application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  #mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
