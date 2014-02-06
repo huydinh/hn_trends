9c0b463c5c83841a208408a0fea138bfd3a9fb15
Huy Dinh <dinhhuydh@gmail.com>
2014-02-06 11:40:42 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140124040802) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: true do |t|
     t.string   "title"
     t.string   "hn_id"
