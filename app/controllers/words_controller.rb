a0024a609b7a11f182d9a3ee8e4846970117d759
huyd <huy.dinh@eastagile.com>
2014-02-26 15:59:39 +0700

    [r] cache the result of calculation

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -11,7 +11,7 @@ class WordsController < ApplicationController
     if params[:from_date].present?
       @ranked_words = Word.ranked_in_range(params[:from_date])
     else
-      @ranked_words = Word.ranked
+      @ranked_words = Rails.cache.fetch('all_ranked_words') { Word.ranked }
     end
     @ranked_words = @ranked_words.take(Word::DEFAULT_LIMIT)
   end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -3,6 +3,7 @@ task :fetch_hn_entries => :environment do
   puts "Fetching HN entries..."
 
   Entry.fetch
+  Rails.cache.write('all_ranked_words', Word.ranked)
 
   puts "done."
 end
